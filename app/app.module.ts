3d981b4b96271abc9f1f8e4f248c534927f8c435
sunpark <yangheat@gmaill.com>
2021-03-31 17:25:01 +0900

    Retrieve shipping prices * Configure Appmodule to use HttpClient

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,5 +1,7 @@
+// 앱 전역에 필요한 심볼과 기능이 등록
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 
@@ -14,6 +16,7 @@ import { CartService } from './cart.service';
 @NgModule({
   imports: [
     BrowserModule,
+    HttpClientModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
